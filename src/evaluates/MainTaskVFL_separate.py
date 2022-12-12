8e31e817a54fd6e5287914982ae5d4c33d73a3b7
LindaLydia <Lydia6125@163.com>
2022-12-12 13:58:28 +0800

    main update to runnable files

--- src/evaluates/MainTaskVFL_separate.py
+++ src/evaluates/MainTaskVFL_separate.py
@@ -13,8 +13,9 @@ import time
 
 from models.vision import resnet18, MLP2
 from utils.basic_functions import cross_entropy_for_onehot, append_exp_res
-from defenses.defense_api import apply_defense
-from defenses.defense_functions import *
+# from evaluates.attacks.attack_api import apply_attack
+from evaluates.defenses.defense_api import apply_defense
+from evaluates.defenses.defense_functions import *
 from utils.constants import *
 import utils.constants as shared_var
 from utils.marvell_functions import KL_gradient_perturb
@@ -40,34 +41,17 @@ class MainTaskVFL_separate(object):
         # self.num_class_list = args.num_class_list
         self.num_classes = args.num_classes
         self.exp_res_dir = args.exp_res_dir
-        # self.gradients_res_a = None
-        # self.gradients_res_b = None
-
-        self.apply_laplace = args.apply_laplace
-        self.apply_gaussian = args.apply_gaussian
-        self.dp_strength = args.dp_strength
-        self.apply_grad_spar = args.apply_grad_spar
-        self.grad_spars = args.grad_spars
-        self.apply_encoder = args.apply_encoder
-        self.ae_lambda = args.ae_lambda
-        self.encoder = args.encoder
-        self.apply_marvell = args.apply_marvell
-        self.marvell_s = args.marvell_s
-        self.apply_discrete_gradients = args.apply_discrete_gradients
-        self.discrete_gradients_bins = args.discrete_gradients_bins
-        # self.discrete_gradients_bound = args.discrete_gradients_bound
-        self.discrete_gradients_bound = 1e-3
-
-        # self.apply_ppdl = args.apply_ppdl
-        # self.ppdl_theta_u = args.ppdl_theta_u
-        # self.apply_gc = args.apply_gc
-        # self.gc_preserved_percent = args.gc_preserved_percent
-        # self.apply_lap_noise = args.apply_lap_noise
-        # self.noise_scale = args.noise_scale
-        # self.apply_discrete_gradients = args.apply_discrete_gradients
 
         self.exp_res_path = args.exp_res_path
         self.parties = args.parties
+
+        self.pred_list = []
+        self.pred_list_clone = []
+        self.pred_gradients_list = []
+        self.pred_gradients_list_clone = []
+        self.loss = None
+        self.train_acc = None
+        self.gt_one_hot_label = None
 
     def label_to_one_hot(self, target, num_classes=10):
         try:
@@ -82,45 +66,48 @@ class MainTaskVFL_separate(object):
         return onehot_target
 
     def train_batch(self, parties_data, batch_label):
-        encoder = self.encoder
-        if self.apply_encoder:
-            if encoder:
-                _, gt_one_hot_label = encoder(batch_label)
-            else:
-                assert(encoder != None)
-        else:
-            gt_one_hot_label = batch_label
+        # encoder = self.encoder
+        # if self.apply_encoder:
+        #     if encoder:
+        #         _, gt_one_hot_label = encoder(batch_label)
+        #     else:
+        #         assert(encoder != None)
+        # else:
+        #     gt_one_hot_label = batch_label
         # print('current_label:', gt_one_hot_label)
+        gt_one_hot_label = batch_label
 
         # ====== normal vertical federated learning ======
 
         # compute logits of clients
-        pred_list = []
-        pred_list_clone = []
+        self.pred_list = []
+        self.pred_list_clone = []
         torch.autograd.set_detect_anomaly(True)
         for ik in range(self.k):
-            pred_list.append(self.parties[ik].local_model(parties_data[ik][0]))
+            self.parties[ik].obtain_local_data(parties_data[ik][0])
+            self.pred_list.append(self.parties[ik].local_model(parties_data[ik][0]))
             # pred_list[ik] = torch.autograd.Variable(pred_list[ik], requires_grad=True).to(self.device)
-            pred_list_clone.append(pred_list[ik].detach().clone())
-            pred_list_clone[ik] = torch.autograd.Variable(pred_list_clone[ik], requires_grad=True).to(self.device)
+            self.pred_list_clone.append(self.pred_list[ik].detach().clone())
+            self.pred_list_clone[ik] = torch.autograd.Variable(self.pred_list_clone[ik], requires_grad=True).to(self.device)
 
         # ######################## aggregate logits of clients ########################
-        pred, loss = self.parties[self.k-1].aggregate(pred_list_clone, gt_one_hot_label)
+        pred, loss = self.parties[self.k-1].aggregate(self.pred_list_clone, gt_one_hot_label)
         # _gradients = torch.autograd.grad(loss, pred, retain_graph=True)
         # _gradients = torch.autograd.grad(loss, pred_list[ik], retain_graph=True)
         # pred = self.parties[self.k-1].global_model(pred_list)
         # loss = self.parties[self.k-1].criterion(pred, gt_one_hot_label)
-        pred_gradients_list, pred_gradients_list_clone = self.parties[self.k-1].gradient_calculation(pred, pred_list_clone, loss)
+        self.pred_gradients_list, self.pred_gradients_list_clone = self.parties[self.k-1].gradient_calculation(pred, self.pred_list_clone, loss)
         # pred, loss, pred_gradients_list, pred_gradients_list_clone = self.parties[self.k-1].aggregate_and_gradient_calculation(pred_list, gt_one_hot_label)
         # ######################## aggregate logits of clients ########################
 
-        pred_gradients_list_clone = apply_defense(self.args, pred_gradients_list_clone)
+        # defense applied on gradients
+        self.pred_gradients_list_clone = self.launch_defense(self.pred_gradients_list_clone, "gradients")        
         
         # print("gradients_clone[ik] have size:", pred_gradients_list_clone[0].size())
         # _g = torch.autograd.grad(pred_list[ik], self.parties[ik].local_model.parameters(), grad_outputs=torch.tensor([[1.]*10]*2048).to(self.device), retain_graph=True)
         # _g = torch.autograd.grad(pred_list[ik], self.parties[ik].local_model.parameters(), grad_outputs=pred_gradients_list_clone[ik], retain_graph=True)
         for ik in range(self.k):
-            self.parties[ik].local_backward(pred_gradients_list_clone[ik], pred_list[ik])
+            self.parties[ik].local_backward(self.pred_gradients_list_clone[ik], self.pred_list[ik])
         self.parties[self.k-1].global_backward(pred, loss)
 
         predict_prob = F.softmax(pred, dim=-1)
@@ -133,7 +120,6 @@ class MainTaskVFL_separate(object):
         print_every = 1
 
         for ik in range(self.k):
-            print("batch_size =",self.batch_size)
             self.parties[ik].prepare_data_loader(batch_size=self.batch_size)
 
         test_acc = 0.0
@@ -151,73 +137,112 @@ class MainTaskVFL_separate(object):
                 self.parties[self.k-1].global_model.train()
 
                 # print("train", parties_data[0][0].size(),parties_data[self.k-1][0].size(),parties_data[self.k-1][1].size())
-                gt_one_hot_label = self.label_to_one_hot(parties_data[self.k-1][1], self.num_classes)
-                gt_one_hot_label = gt_one_hot_label.to(self.device)
+                self.gt_one_hot_label = self.label_to_one_hot(parties_data[self.k-1][1], self.num_classes)
+                self.gt_one_hot_label = self.gt_one_hot_label.to(self.device)
                 # print("parties' data have size:", parties_data[0][0].size(), parties_data[self.k-1][0].size(), parties_data[self.k-1][1].size())
                 # ====== train batch ======
-                loss, train_acc = self.train_batch(parties_data, gt_one_hot_label)
+                self.loss, self.train_acc = self.train_batch(parties_data, self.gt_one_hot_label)
+            
+                print(i,i_epoch)
+                if i == i_epoch == 0:
+                    self.launch_attack(self.pred_gradients_list_clone, self.pred_list_clone, "gradients_label")
+
+            # validation
+            if (i + 1) % print_every == 0:
+                print("validate and test")
+                for ik in range(self.k):
+                    self.parties[ik].local_model.eval()
+                self.parties[self.k-1].global_model.eval()
                 
-                # validation
-                if (i + 1) % print_every == 0:
-                    print("validate and test")
-                    for ik in range(self.k):
-                        self.parties[ik].local_model.eval()
-                    self.parties[self.k-1].global_model.eval()
-                    
-                    suc_cnt = 0
-                    sample_cnt = 0
+                suc_cnt = 0
+                sample_cnt = 0
 
-                    with torch.no_grad():
-                        # enc_result_matrix = np.zeros((self.num_classes, self.num_classes), dtype=int)
-                        # result_matrix = np.zeros((self.num_classes, self.num_classes), dtype=int)
-                        data_loader_list = [self.parties[ik].test_loader for ik in range(self.k)]
-                        # for parties_data in zip(self.parties[0].test_loader, self.parties[self.k-1].test_loader):
-                        for parties_data in zip(*data_loader_list):
-                            # print("test", parties_data[0][0].size(),parties_data[self.k-1][0].size(),parties_data[self.k-1][1].size())
-                            gt_val_one_hot_label = self.label_to_one_hot(parties_data[self.k-1][1], self.num_classes)
-                            gt_val_one_hot_label = gt_val_one_hot_label.to(self.device)
+                with torch.no_grad():
+                    # enc_result_matrix = np.zeros((self.num_classes, self.num_classes), dtype=int)
+                    # result_matrix = np.zeros((self.num_classes, self.num_classes), dtype=int)
+                    data_loader_list = [self.parties[ik].test_loader for ik in range(self.k)]
+                    # for parties_data in zip(self.parties[0].test_loader, self.parties[self.k-1].test_loader):
+                    for parties_data in zip(*data_loader_list):
+                        # print("test", parties_data[0][0].size(),parties_data[self.k-1][0].size(),parties_data[self.k-1][1].size())
+                        gt_val_one_hot_label = self.label_to_one_hot(parties_data[self.k-1][1], self.num_classes)
+                        gt_val_one_hot_label = gt_val_one_hot_label.to(self.device)
 
-                            pred_list = []
-                            for ik in range(self.k):
-                                pred_list.append(self.parties[ik].local_model(parties_data[ik][0]))
-                            test_logit, loss = self.parties[self.k-1].aggregate(pred_list, gt_val_one_hot_label)
+                        pred_list = []
+                        for ik in range(self.k):
+                            pred_list.append(self.parties[ik].local_model(parties_data[ik][0]))
+                        test_logit, test_loss = self.parties[self.k-1].aggregate(pred_list, gt_val_one_hot_label)
 
-                            enc_predict_prob = F.softmax(test_logit, dim=-1)
-                            if self.apply_encoder:
-                                dec_predict_prob = self.encoder.decoder(enc_predict_prob)
-                                predict_label = torch.argmax(dec_predict_prob, dim=-1)
-                            else:
-                                predict_label = torch.argmax(enc_predict_prob, dim=-1)
+                        enc_predict_prob = F.softmax(test_logit, dim=-1)
+                        # if self.apply_encoder:
+                        #     dec_predict_prob = self.encoder.decoder(enc_predict_prob)
+                        #     predict_label = torch.argmax(dec_predict_prob, dim=-1)
+                        # else:
+                        #     predict_label = torch.argmax(enc_predict_prob, dim=-1)
+                        predict_label = torch.argmax(enc_predict_prob, dim=-1)
 
-                            # enc_predict_label = torch.argmax(enc_predict_prob, dim=-1)
-                            actual_label = torch.argmax(gt_val_one_hot_label, dim=-1)
-                            sample_cnt += predict_label.shape[0]
-                            suc_cnt += torch.sum(predict_label == actual_label).item()
-                        test_acc = suc_cnt / float(sample_cnt)
-                        postfix['train_loss'] = loss
-                        postfix['train_acc'] = '{:.2f}%'.format(train_acc * 100)
-                        postfix['test_acc'] = '{:.2f}%'.format(test_acc * 100)
-                        tqdm_train.set_postfix(postfix)
-                        print('Epoch {}% \t train_loss:{:.2f} train_acc:{:.2f} test_acc:{:.2f}'.format(
-                            i_epoch, loss, train_acc, test_acc))
+                        # enc_predict_label = torch.argmax(enc_predict_prob, dim=-1)
+                        actual_label = torch.argmax(gt_val_one_hot_label, dim=-1)
+                        sample_cnt += predict_label.shape[0]
+                        suc_cnt += torch.sum(predict_label == actual_label).item()
+                    self.test_acc = suc_cnt / float(sample_cnt)
+                    postfix['train_loss'] = self.loss
+                    postfix['train_acc'] = '{:.2f}%'.format(self.train_acc * 100)
+                    postfix['test_acc'] = '{:.2f}%'.format(self.test_acc * 100)
+                    tqdm_train.set_postfix(postfix)
+                    print('Epoch {}% \t train_loss:{:.2f} train_acc:{:.2f} test_acc:{:.2f}'.format(
+                        i_epoch, self.loss, self.train_acc, self.test_acc))
         
-        parameter = 'none'
-        if self.apply_laplace or self.apply_gaussian:
-            parameter = str(self.dp_strength)
-        elif self.apply_grad_spar:
-            parameter = str(self.grad_spars)
-        elif self.apply_encoder:
-            parameter = str(self.ae_lambda)
-        elif self.apply_discrete_gradients:
-            parameter = str(self.discrete_gradients_bins)
-        elif self.apply_marvell:
-            parameter = str(self.marvell_s)
+        # parameter = 'none'
+        # if self.apply_laplace or self.apply_gaussian:
+        #     parameter = str(self.dp_strength)
+        # elif self.apply_grad_spar:
+        #     parameter = str(self.grad_spars)
+        # elif self.apply_encoder:
+        #     parameter = str(self.ae_lambda)
+        # elif self.apply_discrete_gradients:
+        #     parameter = str(self.discrete_gradients_bins)
+        # elif self.apply_marvell:
+        #     parameter = str(self.marvell_s)
 
-        if self.apply_laplace or self.apply_gaussian or self.apply_grad_spar or self.apply_encoder or self.apply_marvell:
-            exp_result = str(parameter) + ' ' + str(test_acc) + ' bs=' + str(self.batch_size) + '|num_class=' + str(self.num_classes)
-        else:
-            exp_result = f"bs|num_class|epochs|recovery_rate,%d|%d|%d| %lf" % (self.batch_size, self.num_classes, self.self.epochs, test_acc)
-        append_exp_res(self.exp_res_path+'main_task.txt', exp_result)
+        # if self.apply_laplace or self.apply_gaussian or self.apply_grad_spar or self.apply_encoder or self.apply_marvell:
+        #     exp_result = str(parameter) + ' ' + str(test_acc) + ' bs=' + str(self.batch_size) + '|num_class=' + str(self.num_classes)
+        # else:
+        #     exp_result = f"bs|num_class|epochs|recovery_rate,%d|%d|%d| %lf" % (self.batch_size, self.num_classes, self.self.epochs, test_acc)
+        
+        exp_result = f"bs|num_class|epochs|recovery_rate,%d|%d|%d| %lf" % (self.batch_size, self.num_classes, self.epochs, self.test_acc)
+        append_exp_res(self.exp_res_path, exp_result)
         print(exp_result)
         
-        return test_acc, parameter
+        return test_acc
+
+
+    def launch_attack(self, gradients_list, pred_list, type):
+        if type == 'gradients_label':
+            for ik in range(self.k):
+                start_time = time.time()
+                recovery_history = self.parties[ik].party_attack(self.args, gradients_list[ik], pred_list[ik])
+                end_time = time.time()
+                if recovery_history != None:
+                    recovery_rate_history = []
+                    for dummy_label in recovery_history:
+                        rec_rate = self.calc_label_recovery_rate(dummy_label, self.gt_one_hot_label)
+                        recovery_rate_history.append(rec_rate)
+                        print(f'batch_size=%d,class_num=%d,party_index=%d,recovery_rate=%lf,time_used=%lf' % (dummy_label.size()[0], self.num_classes, ik, rec_rate, end_time - start_time))
+                    best_rec_rate = max(recovery_rate_history)
+                    exp_result = f"bs|num_class|attack_party_index|recovery_rate,%d|%d|%d|%lf|%s" % (dummy_label.size()[0], self.num_classes, ik, best_rec_rate, str(recovery_rate_history))
+                    append_exp_res(self.parties[ik].attacker.exp_res_path, exp_result)
+        else:
+            # further extention
+            pass
+
+    def launch_defense(self, gradients_list, type):
+        if type == 'gradients':
+            return apply_defense(self.args, gradients_list)
+        else:
+            # further extention
+            pass
+
+    def calc_label_recovery_rate(self, dummy_label, gt_label):
+        success = torch.sum(torch.argmax(dummy_label, dim=-1) == torch.argmax(gt_label, dim=-1)).item()
+        total = dummy_label.shape[0]
+        return success / total
