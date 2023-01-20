e14ca75907df2eaaf9d6228720041738a0f2ebbd
ZixuanGu <122776676+ZixuanGu@users.noreply.github.com>
2023-01-20 11:15:59 +0800

    Update MainTaskVFL.py
    add FedBCD

--- src/evaluates/MainTaskVFL.py
+++ src/evaluates/MainTaskVFL.py
@@ -46,6 +46,8 @@ class MainTaskVFL(object):
 
         self.exp_res_path = args.exp_res_path
         self.parties = args.parties
+        
+        self.Q = args.Q # FedBCD
 
         self.parties_data = None
         self.gt_one_hot_label = None
@@ -53,6 +55,13 @@ class MainTaskVFL(object):
         self.pred_list_clone = []
         self.pred_gradients_list = []
         self.pred_gradients_list_clone = []
+        
+        # FedBCD related
+        self.local_pred_list = []
+        self.local_pred_list_clone = []
+        self.local_pred_gradients_list = []
+        self.local_pred_gradients_list_clone = []
+        
         self.loss = None
         self.train_acc = None
 
@@ -83,38 +92,60 @@ class MainTaskVFL(object):
         # print('current_label:', gt_one_hot_label)
 
         # ====== normal vertical federated learning ======
-
-        # compute logits of clients
         self.pred_list = []
         self.pred_list_clone = []
         torch.autograd.set_detect_anomaly(True)
-        for ik in range(self.k):
-            self.parties[ik].obtain_local_data(parties_data[ik][0])
-            self.pred_list.append(self.parties[ik].local_model(parties_data[ik][0]))
-            # pred_list[ik] = torch.autograd.Variable(pred_list[ik], requires_grad=True).to(self.device)
-            self.pred_list_clone.append(self.pred_list[ik].detach().clone())
-            self.pred_list_clone[ik] = torch.autograd.Variable(self.pred_list_clone[ik], requires_grad=True).to(self.device)
+        # == FedBCD ==
+        for q in range(self.Q):
+            for ik in range(self.k):
+                if q == 0: # before first iteration, Exchange information between party 1,2...k
+                    # compute logits of clients
+                    self.parties[ik].obtain_local_data(parties_data[ik][0])
+                    self.pred_list.append(self.parties[ik].local_model(parties_data[ik][0]))
+                    self.pred_list_clone.append(self.pred_list[ik].detach().clone())
+                    self.pred_list_clone[ik] = torch.autograd.Variable(self.pred_list_clone[ik], requires_grad=True).to(self.device)
+                
+                    # ######################## aggregate logits of clients ########################
+                    pred, loss = self.parties[self.k-1].aggregate(self.pred_list_clone, gt_one_hot_label)
+                    self.pred_gradients_list, self.pred_gradients_list_clone = self.parties[self.k-1].gradient_calculation(pred, self.pred_list_clone, loss)
+                    # ######################## aggregate logits of clients ########################
+                    
+                    # defense applied on gradients
+                    if self.args.apply_defense == True and self.args.apply_mid == False and self.args.apply_cae == False:
+                        self.pred_gradients_list_clone = self.launch_defense(self.pred_gradients_list_clone, "gradients")        
+                    
+                    # update parameters for all parties
+                    for ik in range(self.k):
+                        self.parties[ik].local_backward(self.pred_gradients_list_clone[ik], self.pred_list[ik])
+                    self.parties[self.k-1].global_backward(pred, loss)
+                
+                else: # FedBCD: in other iterations, no communication happen, no defense&attack
+                    if ik == self.k-1:
+                        ############### Party k: uses old data transfered from the other parties ###############
+                        # the intermediate information obtained from the most recent synchronization: 
+                        self.local_pred_list = self.pred_list
+                        self.local_pred_list_clone = self.pred_list_clone
+                        # update logits of Party k
+                        self.local_pred_list[self.k-1] = self.parties[self.k-1].local_model(parties_data[self.k-1][0])
+                        self.local_pred_list_clone[self.k-1] = self.local_pred_list[[self.k-1]].detach().clone()
+                        self.local_pred_list_clone[[self.k-1]] = torch.autograd.Variable(self.local_pred_list_clone[[self.k-1]], requires_grad=True).to(self.device)
+                        # Party k: aggregate logits of clients ####
+                        pred, loss = self.parties[self.k-1].aggregate(self.local_pred_list_clone, gt_one_hot_label)
+                        self.local_pred_gradients_list, self.local_pred_gradients_list_clone = self.parties[self.k-1].gradient_calculation(pred, self.pred_list_clone, loss)
+                        
+                        # update parameters for Party k
+                        self.parties[self.k-1].local_backward(self.local_pred_gradients_list_clone[self.k-1], self.pred_list[self.k-1])
+                        self.parties[self.k-1].global_backward(pred, loss)
 
-        # ######################## aggregate logits of clients ########################
-        pred, loss = self.parties[self.k-1].aggregate(self.pred_list_clone, gt_one_hot_label)
-        # _gradients = torch.autograd.grad(loss, pred, retain_graph=True)
-        # _gradients = torch.autograd.grad(loss, pred_list[ik], retain_graph=True)
-        # pred = self.parties[self.k-1].global_model(pred_list)
-        # loss = self.parties[self.k-1].criterion(pred, gt_one_hot_label)
-        self.pred_gradients_list, self.pred_gradients_list_clone = self.parties[self.k-1].gradient_calculation(pred, self.pred_list_clone, loss)
-        # pred, loss, pred_gradients_list, pred_gradients_list_clone = self.parties[self.k-1].aggregate_and_gradient_calculation(pred_list, gt_one_hot_label)
-        # ######################## aggregate logits of clients ########################
+                    else:
+                        ############### Party 1~k-1: use the original gradient ###############
+                        # the intermediate information obtained from the most recent synchronization: 
+                        # self.pred_gradients_list_clone
 
-        # defense applied on gradients
-        if self.args.apply_defense == True and self.args.apply_mid == False and self.args.apply_cae == False:
-            self.pred_gradients_list_clone = self.launch_defense(self.pred_gradients_list_clone, "gradients")        
-
-        # print("gradients_clone[ik] have size:", pred_gradients_list_clone[0].size())
-        # _g = torch.autograd.grad(pred_list[ik], self.parties[ik].local_model.parameters(), grad_outputs=torch.tensor([[1.]*10]*2048).to(self.device), retain_graph=True)
-        # _g = torch.autograd.grad(pred_list[ik], self.parties[ik].local_model.parameters(), grad_outputs=pred_gradients_list_clone[ik], retain_graph=True)
-        for ik in range(self.k):
-            self.parties[ik].local_backward(self.pred_gradients_list_clone[ik], self.pred_list[ik])
-        self.parties[self.k-1].global_backward(pred, loss)
+                        # update parameters for Party 1~k-1
+                        for ik in range(self.k-1):
+                            local_pred = self.parties[ik].local_model(parties_data[ik][0])
+                            self.parties[ik].local_backward(self.pred_gradients_list_clone[ik], local_pred)
 
         predict_prob = F.softmax(pred, dim=-1)
         if self.args.apply_cae:
