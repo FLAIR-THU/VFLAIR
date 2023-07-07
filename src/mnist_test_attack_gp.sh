b498f0a7fce0afd593c8fa8913dbdd3cfda69c55
LindaLydia <Lydia6125@163.com>
2023-07-07 10:54:02 +0800

    change file name in sh files

--- src/mnist_test_attack_gp.sh
+++ src/mnist_test_attack_gp.sh
@@ -7,27 +7,27 @@
 echo 'GradPerturb main Begin'
 # #SBATCH --qos high
 # DP 0.01
-# python main_separate.py --configs main/mnist_test_attack_gp --gpu 1
+# python main_pipeline.py --configs main/mnist_test_attack_gp --gpu 1
 
 # DP 0.1
 sed -i 's/"perturb_epsilon": 0.01/"perturb_epsilon": 0.1/g' ./configs/main/mnist_test_attack_gp.json
-# python main_separate.py --configs main/mnist_test_attack_gp --gpu 1
+# python main_pipeline.py --configs main/mnist_test_attack_gp --gpu 1
 
 # DP 0.3
 sed -i 's/"perturb_epsilon": 0.1/"perturb_epsilon": 0.3/g' ./configs/main/mnist_test_attack_gp.json
-python main_separate.py --configs main/mnist_test_attack_gp --gpu 1
+python main_pipeline.py --configs main/mnist_test_attack_gp --gpu 1
 
 # DP 1.0
 sed -i 's/"perturb_epsilon": 0.3/"perturb_epsilon": 1.0/g' ./configs/main/mnist_test_attack_gp.json
-# python main_separate.py --configs main/mnist_test_attack_gp --gpu 1
+# python main_pipeline.py --configs main/mnist_test_attack_gp --gpu 1
 
 # DP 3.0
 sed -i 's/"perturb_epsilon": 1.0/"perturb_epsilon": 3.0/g' ./configs/main/mnist_test_attack_gp.json
-# python main_separate.py --configs main/mnist_test_attack_gp --gpu 1
+# python main_pipeline.py --configs main/mnist_test_attack_gp --gpu 1
 
 # DP 10.0
 sed -i 's/"perturb_epsilon": 3.0/"perturb_epsilon": 10.0/g' ./configs/main/mnist_test_attack_gp.json
-# python main_separate.py --configs main/mnist_test_attack_gp --gpu 1
+# python main_pipeline.py --configs main/mnist_test_attack_gp --gpu 1
 
 sed -i 's/"perturb_epsilon": 10.0/"perturb_epsilon": 0.01/g' ./configs/main/mnist_test_attack_gp.json
 
@@ -37,27 +37,27 @@ echo 'GradPerturb End'
 # echo 'GradPerturb main Begin'
 # # #SBATCH --qos high
 # # DP 0.01
-# # python main_separate.py --configs main_split/mnist_test_attack_gp --gpu 1
+# # python main_pipeline.py --configs main_split/mnist_test_attack_gp --gpu 1
 
 # # DP 0.1
 # sed -i 's/"perturb_epsilon": 0.01/"perturb_epsilon": 0.1/g' ./configs/main_split/mnist_test_attack_gp.json
-# # python main_separate.py --configs main_split/mnist_test_attack_gp --gpu 1
+# # python main_pipeline.py --configs main_split/mnist_test_attack_gp --gpu 1
 
 # # DP 0.3
 # sed -i 's/"perturb_epsilon": 0.1/"perturb_epsilon": 0.3/g' ./configs/main_split/mnist_test_attack_gp.json
-# # python main_separate.py --configs main_split/mnist_test_attack_gp --gpu 1
+# # python main_pipeline.py --configs main_split/mnist_test_attack_gp --gpu 1
 
 # # DP 1.0
 # sed -i 's/"perturb_epsilon": 0.3/"perturb_epsilon": 1.0/g' ./configs/main_split/mnist_test_attack_gp.json
-# # python main_separate.py --configs main_split/mnist_test_attack_gp --gpu 1
+# # python main_pipeline.py --configs main_split/mnist_test_attack_gp --gpu 1
 
 # # DP 3.0
 # sed -i 's/"perturb_epsilon": 1.0/"perturb_epsilon": 3.0/g' ./configs/main_split/mnist_test_attack_gp.json
-# # python main_separate.py --configs main_split/mnist_test_attack_gp --gpu 1
+# # python main_pipeline.py --configs main_split/mnist_test_attack_gp --gpu 1
 
 # # DP 10.0
 # sed -i 's/"perturb_epsilon": 3.0/"perturb_epsilon": 10.0/g' ./configs/main_split/mnist_test_attack_gp.json
-# # python main_separate.py --configs main_split/mnist_test_attack_gp --gpu 1
+# # python main_pipeline.py --configs main_split/mnist_test_attack_gp --gpu 1
 
 # sed -i 's/"perturb_epsilon": 10.0/"perturb_epsilon": 0.01/g' ./configs/main_split/mnist_test_attack_gp.json
 
--- src/mnist_test_attack_mid.sh
+++ src/mnist_test_attack_mid.sh
@@ -5,32 +5,32 @@
 
 echo 'MID agg main begin' #SBATCH --qos high
 
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 0.0/"lambda": 1e-8/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 1e-8/"lambda": 1e-6/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 1e-6/"lambda": 0.0001/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 0.0001/"lambda": 0.01/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 0.01/"lambda": 0.1/g' ./configs/main_mnist/mnist_test_attack_mid.json
 echo '0.1'
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 0.1/"lambda": 1.0/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 1.0/"lambda": 100/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 100/"lambda": 10000/g' ./configs/main_mnist/mnist_test_attack_mid.json
-python main_separate.py --configs main_mnist/mnist_test_attack_mid --gpu 1
+python main_pipeline.py --configs main_mnist/mnist_test_attack_mid --gpu 1
 
 sed -i 's/"lambda": 10000/"lambda": 0.0/g' ./configs/main_mnist/mnist_test_attack_mid.json
 
