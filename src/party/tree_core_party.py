aecd59907828ef8e1be027d64ef785c331cff51e
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-28 16:49:58 +0900

    patch

--- src/party/tree_core_party.py
+++ src/party/tree_core_party.py
@@ -51,7 +51,11 @@ class Party:
     def decrypt_1dlist(self, row):
         results = []
         for e in row:
-            results.append(self.sk.decrypt(e))
+            try:
+                results.append(self.sk.decrypt(e))
+            except Exception as e:
+                print(type(e), e.args, e.message, str(e))
+                continue
         return results
 
     def decrypt_2dlist(self, mat):
