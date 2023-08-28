fa4518789446662708a33a03e830c6eb0e0c14bf
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-28 16:42:04 +0900

    update

--- src/tree.sh
+++ src/tree.sh
@@ -5,29 +5,37 @@ done
 
 for seed in 1 2 3 4 5
 do
+    python3 main_tree.py --dataset nursery --configs tree/basic_configs_secureforest --seed ${seed} >> tmpoutput/output_nsr.out &
+done
+
+for seed in 1 2 3 4 5
+do
     python3 main_tree.py --dataset nursery --configs tree/basic_configs_xgboost --seed ${seed} >> tmpoutput/output_nx.out &
 done
 
 for seed in 1 2 3 4 5
 do
-    python3 main_tree.py --dataset nursery --configs tree/basic_configs_secureboost --seed ${seed} >> tmpoutput/output_ns.out &
+    python3 main_tree.py --dataset nursery --configs tree/basic_configs_secureboost --seed ${seed} >> tmpoutput/output_nsx.out &
 done
-echo "Nursery - Secureboost" & wait
+echo "Nursery" & wait
 
 for seed in 1 2 3 4 5
 do
     python3 main_tree.py --dataset credit --configs tree/basic_configs_randomforest --seed ${seed} --grid >> tmpoutput/output_cr.out &
 done
-echo "Credit - RandomForest"
+
+for seed in 1 2 3 4 5
+do
+    python3 main_tree.py --dataset credit --configs tree/basic_configs_secureforest --seed ${seed} --grid >> tmpoutput/output_csr.out &
+done
 
 for seed in 1 2 3 4 5
 do
     python3 main_tree.py --dataset credit --configs tree/basic_configs_xgboost --seed ${seed} --grid >> tmpoutput/output_cx.out &
 done
-echo "Credit - XGBoost"
 
 for seed in 1 2 3 4 5
 do
-    python3 main_tree.py --dataset credit --configs tree/basic_configs_secureboost --seed ${seed} --grid >> tmpoutput/output_cs.out &
+    python3 main_tree.py --dataset credit --configs tree/basic_configs_secureboost --seed ${seed} --grid >> tmpoutput/output_csx.out &
 done
-echo "Credit - Secureboost" & wait
+echo "Credit" & wait
